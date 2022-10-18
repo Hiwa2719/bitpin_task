19d92170854966ced38fcc21e5ddaf1c6e10a3ef
hiwa2719 <hiahmadyan@gmail.com>
2022-10-18 14:49:45 +0330

    adding Article and Rating models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+User = get_user_model()
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+def rating_validator(value):
+    if value not in range(0,6):
+        raise ValidationError('rating must be in range of 0 to 5')
+
+class Rating(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[rating_validator])
+
+    def __str__(self):
+        return f'{self.article}-{self.user}-{self.rating}'
